aff0c3ecc367efb7f1373168e24fab5aaf0ebc7e
Kuerl <kuerl14@gmail.com>
2021-04-23 02:25:49 +0700

    control LED

--- SmartHomeV3RasSide/util/bulb.py
+++ SmartHomeV3RasSide/util/bulb.py
@@ -1,3 +1,28 @@
+# Hardware Code
+try:
+    from gpiozero import LED
+    from time import sleep
+except Exception as e:
+    print('Hardware Lib Error: {}'.format(e))
+
+try:
+    led = LED(17)
+except Exception as e:
+    print('LED Error: ', e)
+
+def turnOn():
+    try:
+        led.on()
+    except Exception as e:
+        print('Bulb Process Error: ',e)
+
+def turnOff():
+    try:
+        led.off()
+    except Exception as e:
+        print('Bulb Process Error: ',e)
+
+# Code
 def controlBulb(param):
     try:
         param = int(param)
@@ -5,9 +30,11 @@ def controlBulb(param):
         if active == 0:
             order = (param - active)/10
             print('Light: ',int(order),' is turn off')
+            turnOff()
         elif active == 1:
             order = (param - active)/10
             print('Light: ',int(order),' is turn on')
+            turnOn()
         else:
             print('Not Valid Param')
     except Exception as e:
