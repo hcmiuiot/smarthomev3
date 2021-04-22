44b788bc0395b6d5c81dbe81ce70e69020ffa597
Kuerl <kuerl14@gmail.com>
2021-04-23 06:24:29 +0700

    led optimize debug

--- SmartHomeV3RasSide/util/bulb.py
+++ SmartHomeV3RasSide/util/bulb.py
@@ -5,25 +5,19 @@ try:
 except Exception as e:
     print('Hardware Lib Error: {}'.format(e))
 
-def turnOn(led):
+led = LED(17)
+
+def turnOn():
     try:
         led.on()
     except Exception as e:
         print('Bulb Process Error: ',e)
 
-def turnOff(led):
+def turnOff():
     try:
         led.off()
     except Exception as e:
         print('Bulb Process Error: ',e)
-
-def turnLight(plugLED, bool):
-    led = LED(plugLED)
-    if bool == 0:
-        turnOff(led)
-    if bool == 1:
-        turnOn(led)
-    print(led)
 
 # Util Code
 def controlBulb(param):
@@ -31,17 +25,13 @@ def controlBulb(param):
         param = int(param)
         active = param%10
         order = (param - active)/10
-        if order == 1:
-            plug = 17
-        else:
-            plug = 18 # Bulb 2
         if active == 0:
             print('Light: ',int(order),' is turn off')
+            turnOff()
         elif active == 1:
             print('Light: ',int(order),' is turn on')
-            turnOn(plug)
+            turnOn()
         else:
             print('Not Valid Param')
-        turnLight(plug, active)
     except Exception as e:
         print('Bulb Error: ', e)
