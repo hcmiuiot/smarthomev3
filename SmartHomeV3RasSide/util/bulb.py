39b34fdcd063201859ee85ec7d2e84cf00cd00f0
Kuerl <kuerl14@gmail.com>
2021-04-23 06:32:13 +0700

    led optimize debug

--- SmartHomeV3RasSide/util/bulb.py
+++ SmartHomeV3RasSide/util/bulb.py
@@ -1,21 +1,23 @@
 # Hardware Code
 try:
-    from gpiozero import LED
-    from time import sleep
+    import RPi.GPIO as GPIO
 except Exception as e:
     print('Hardware Lib Error: {}'.format(e))
 
-led = LED(17)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-def turnOn():
+def turnOn(plug):
     try:
-        led.on()
+        GPIO.setup(plug,GPIO.OUT)
+        GPIO.setup(plug,GPIO.OUT)
     except Exception as e:
         print('Bulb Process Error: ',e)
 
-def turnOff():
+def turnOff(plug):
     try:
-        led.off()
+        GPIO.setup(plug,GPIO.OUT)
+        GPIO.setup(plug,GPIO.OUT)
     except Exception as e:
         print('Bulb Process Error: ',e)
 
@@ -25,12 +27,16 @@ def controlBulb(param):
         param = int(param)
         active = param%10
         order = (param - active)/10
+        if order == 1:
+            plug = 17
+        if order = 2:
+            plug = 18
         if active == 0:
             print('Light: ',int(order),' is turn off')
-            turnOff()
+            turnOff(plug)
         elif active == 1:
             print('Light: ',int(order),' is turn on')
-            turnOn()
+            turnOn(plug)
         else:
             print('Not Valid Param')
     except Exception as e:
